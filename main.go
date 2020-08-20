abe501f205277b84bff7ca2b18a47d2b5e3ecc00
ningwei34 <56311681+ningwei34@users.noreply.github.com>
2020-08-21 00:23:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -50,11 +50,11 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				quota, err := bot.GetMessageQuota().Do()
+				_, err := bot.GetMessageQuota().Do()
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Hello"+message.Text+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Hello"+message.Text)).Do(); err != nil {
 					log.Print(err)
 				}
 			}
